44e121f236897a57f4f2fc0a9e50296ff3a01c8c
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-11-08 00:21:37 +0100

    Assigned principal, rate and time

--- Yusuf_Son.py
+++ Yusuf_Son.py
@@ -1,2 +1,5 @@
 print("Welcome to Yusuf & Sons Ltd".upper().center(80))
 
+principal = float(input("Enter your Principal(₦): "))
+rate = float(input("Enter a rate(%): "))
+time = float(input("Enter a time(years): "))
