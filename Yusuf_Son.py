32a39aa905d583de18639fd63dc505b7e516ff2d
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-11-08 00:33:51 +0100

    Wrapped up SI

--- Yusuf_Son.py
+++ Yusuf_Son.py
@@ -4,4 +4,6 @@ principal = float(input("Enter your Principal(₦): "))
 rate = float(input("Enter a rate(%): "))
 time = float(input("Enter a time(years): "))
 
-simple_interest = (principal * rate * time)/100
+simple_interest = "₦" + str((principal * rate * time)/100)
+
+print("Simple Interest = PRT/100 = " + "(" + str(principal) + " × " + str(rate) + " × " + str(time) + ")" + "/100 = " + simple_interest)
