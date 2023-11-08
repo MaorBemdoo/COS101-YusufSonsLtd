7471708cbab8d778294fde67f842cd04d1d1003e
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-11-08 21:04:09 +0100

    Worked on compound interest

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ rate = float(input("Enter a rate(%): "))
 time = float(input("Enter a time(years): "))
 
 simple_interest = "₦" + str((principal * rate * time)/100)
+compound_interest = "₦" + str((principal * (1 + (rate/100)) ** time) - principal)
 
 print("\nSimple Interest = PRT/100 = " + "(" + str(principal) + " × " + str(rate) + " × " + str(time) + ")" + "/100 = " + simple_interest)
+print("\nCompound Interest = P(1+(r/100))-P = " + str(principal) + "(" + str(1) + " + " + "(" + str(rate) + ")" + "/100)" + " - " + str(principal))
