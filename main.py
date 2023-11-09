ef283de10895eb0a6779f6279b28ae5c260e433d
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-11-09 09:14:54 +0100

    Updated S.I and C.I formula

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 print("Welcome to Yusuf & Sons Ltd".upper().center(80))
 
-principal = float(input("Enter your Principal(₦): "))
-rate = float(input("Enter a rate(%): "))
+principal = float(input("Enter your initial amount(₦): "))
+interest_rate = float(input("Enter an interest rate(%): "))
+n = int(input("Enter number of times interest applied per time period: "))
 time = float(input("Enter a time(years): "))
 
-simple_interest = "₦" + str((principal * rate * time)/100)
-compound_interest = "₦" + str((principal * (1 + (rate/100)) ** time) - principal)
+simple_interest = "₦" + str((principal * ( 1 + ( interest_rate * time))))
+compound_interest = "₦" + str(principal * (1 + (interest_rate/n)) ** (n * time))
 
-print("\nSimple Interest = PRT/100 = " + "(" + str(principal) + " × " + str(rate) + " × " + str(time) + ")" + "/100 = " + simple_interest)
+print("\nSimple Interest = P(1 + rt) = " + "(" + str(principal) + " × " + str(rate) + " × " + str(time) + ")" + "/100 = " + simple_interest)
 print("\nCompound Interest = P(1+(r/100))-P = " + str(principal) + "(" + str(1) + " + " + "(" + str(rate) + ")" + "/100)" + " - " + str(principal))
